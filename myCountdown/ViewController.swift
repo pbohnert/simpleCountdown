1799c016c505165a8b58f9b463a820ab9ace90bb
Peter Bohnert <pbohnert@gmail.com>
2014-11-02 07:57:29 -0800

    Initial Commit

---
+++ myCountdown/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myCountdown
+//
+//  Created by Peter Bohnert on 11/2/14.
+//  Copyright (c) 2014 Blue Lotus Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
